06dacc786b5547a3aa6ceb5bf4b9ef56b786661a
aaug <aaug@caasco.ca>
2023-05-10 21:36:02 -0400

    static content

--- src/data.js
+++ src/data.js
@@ -1,35 +1,21 @@
 const content = {
   siteName: 'Moonshine Unisex Hair Salon',
-  sitePhoto: 'https://moonshinehairsalon.com/img/moonshine.jpg',
   contact: {
-    address: {
-      streetNumber: 1749,
-      unitNumber: 1,
-      street: 'Jane St.',
-      city: 'North York',
-      province: 'ON',
-      postalCode: 'M9N 2S5',
-    },
-    phone: '416.364.8544',
-    email: 'info@moonshinehairsalon.com',
     social: {
       faceBook: {
         icon: 'https://moonshinehairsalon.com/img/facebook.png',
-        url: 'https://www.facebook.com/MoonshineHairSalon',
       },
       twitter: {
         icon: 'https://moonshinehairsalon.com/img/twitter.png',
-        url: 'https://twitter.com/MoonshineSalon',
       },
       instagram: {
         icon: 'https://moonshinehairsalon.com/img/twitter.png',
-        url: 'https://www.instagram.com/moonshinesalon/',
       }
     }
   },
   main: {
     services: ['Curls', 'Texturizing', 'Braids', 'Twists', 'Weave', 'Relaxer', 'Soft (Jheri) Curls', 'Colouring', 'Perm', 'Hot Press', 'Highlights', 'Creative Men\'s Cut', 'Boy\'s Cut', 'Kid\'s Hairdo'],
-  }
+  },
 };
 
 export default content;
